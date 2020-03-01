e3c967ee5fb79523725693394ee4c29a3bccdb8c
Jose Bautista <jlbautistavelasquez@gmail.com>
2020-03-01 10:03:00 -0500

    Creado activity Login

---
+++ app/src/main/java/com/cibertec/appcliente/activities/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.cibertec.appcliente.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cibertec.appcliente.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
